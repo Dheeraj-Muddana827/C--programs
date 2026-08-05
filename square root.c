48157608fb816dae2882f3753849006d97ec6dc8
Dheeraj-Muddana827 <25pa1a4268@vishnu.edu.in>
2026-08-05 11:34:40 +0530

    Add files via upload

---
+++ square root.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+float n,result;
+ printf("enter the value n:");
+ scanf("%f",&n);
+ result=sqrt(n);
+ printf("square root=%f",result);
+ 
+ return 0;
+ 
+  }
