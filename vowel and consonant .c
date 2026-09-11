a358f8786887864cd1349a8bfe80ff4b383f7c59
Dheeraj-Muddana <25pa1a4268@vishnu.edu.in>
2026-09-11 21:37:40 +0530

    Add files via upload

---
+++ vowel and consonant .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{
+char ch;
+printf("enter a letter:");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'
+||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+printf("vowel");
+}
+else
+{
+printf("consonant");
+}
+return 0;
+}
